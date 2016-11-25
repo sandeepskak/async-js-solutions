799309c031690c863d7394a206591b0e9c66e188
sandeep <653sandeep@gmail.com>
2016-11-25 15:33:22 +0530

    Solutions for ex1 and ex2

--- ex1.js
+++ ex1.js
@@ -19,10 +19,28 @@ function output(text) {
 
 function getFile(file) {
 	fakeAjax(file,function(text){
-		
+		handleResponse(file,text);
 	});
 }
-
+var responses = {};
+function handleResponse(filename, content) {
+  if (!responses[filename]) {
+		responses[filename] = content;
+	}
+	var filenames = ["file1","file2","file3"];
+	for (var i=0; i<filenames.length; i++) {
+		if (filenames[i] in responses) {
+			if (responses[filenames[i]] !== true) {
+				output(responses[filenames[i]]);
+				responses[filenames[i]] = true;
+			}
+		}
+		else {
+			return false;
+		}
+	}
+	output("Complete!");
+}
 // request all files at once in "parallel"
 getFile("file1");
 getFile("file2");
